e0a157bd9d0e34b4039dce3c295597c16a0bace5
akhilou <akhilmyaka99@gmail.com>
2025-02-12 15:10:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+}
+
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
