a0779d9e613c14939d2e87885274b7ecb991dfde
akhilou <akhilmyaka99@gmail.com>
2025-02-12 15:30:21 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
 }
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
