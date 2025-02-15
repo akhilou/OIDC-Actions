f4ffb99f14d7e79656b2e8efb4b76b548dfd5f34
akhilou <akhilmyaka99@gmail.com>
2025-02-15 22:49:12 +0530

    Delete modules directory

--- modules/subnet/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "The ID of the VPC"
-  type        = string
-}
-
-variable "subnet_cidr" {
-  description = "The CIDR block for the subnet"
-  type        = string
-}
