1f124ad8bc581e277658d7c4bced6bc736d1c847
akhilou <akhilmyaka99@gmail.com>
2025-02-17 11:00:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "myaws-bucket-terraform-state-file"
     key    = "terraform-state-file/statefile"
-    region = "var.region"
+    region = var.region
   }
 }
 
@@ -11,7 +11,43 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
-  cidr_block = var.vpc_cidr
-  vpc_name = var.vpc_name
+  source    = "./modules/vpc"
+  vpc_cidr  = var.vpc_cidr
+  vpc_name  = var.vpc_name
+}
+
+module "subnets" {
+  source       = "./modules/subnets"
+  vpc_id       = module.vpc.vpc_id
+  subnet_cidrs = var.subnet_cidrs
+}
+
+module "internet_gateway" {
+  source = "./modules/internet_gateway"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "route_table" {
+  source     = "./modules/route_table"
+  vpc_id     = module.vpc.vpc_id
+  gateway_id = module.internet_gateway.internet_gateway_id
+}
+
+module "route_table_association" {
+  source         = "./modules/route_table_association"
+  subnet_ids     = module.subnets.subnet_ids
+  route_table_id = module.route_table.route_table_id
+}
+
+module "security_group" {
+  source = "./modules/security_group"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "alb" {
+  source            = "./modules/alb"
+  vpc_id            = module.vpc.vpc_id
+  subnet_ids        = module.subnets.subnet_ids
+  alb_name          = var.alb_name
+  security_group_id = module.security_group.security_group_id
 }
