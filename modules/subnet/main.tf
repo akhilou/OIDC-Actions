709583140d07762de5023f95a235b6d419a6d6f5
akhilou <akhilmyaka99@gmail.com>
2025-02-17 11:02:43 +0530

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,9 +1,11 @@
 resource "aws_subnet" "this" {
+  count = length(var.subnet_cidrs)
+
   vpc_id     = var.vpc_id
-  cidr_block = var.subnet_cidr
+  cidr_block = var.subnet_cidrs[count.index]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "main-subnet"
+    Name = "main-subnet-${count.index}"
   }
 }
