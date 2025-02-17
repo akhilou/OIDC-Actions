2f494bdf5454feaaaffd67efde37b9fc23aca245
akhilou <akhilmyaka99@gmail.com>
2025-02-17 11:11:08 +0530

    Create main.tf

---
+++ modules/security_group/main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow_all_tcp" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all_tcp"
+  }
+}
